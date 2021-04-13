27109ac2ff9ea33b1b99475114185007438a0f38
Sajad <sajadalipour@aut.ac.ir>
2021-04-13 20:05:16 +0430

    Ghaar detection for photos implemented

--- db.py
+++ db.py
@@ -1,4 +1,15 @@
 import pymysql
+import traceback
+
+
+class ForwardedMessage:
+
+    def __init__(self, group_id, message_id, caption, image_hash, video_hash):
+        self.group_id = group_id
+        self.message_id = message_id
+        self.caption = caption
+        self.image_hash = image_hash
+        self.video_hash = video_hash
 
 
 def connect(host, user, password, db):
@@ -6,48 +17,175 @@ def connect(host, user, password, db):
     return conn
 
 
-def commit(func):
-    def wrapper_func(conn, group_id, user_id):
-        func(conn, group_id, user_id)
-        conn.commit()
-        conn.cursor().close()
-    return wrapper_func
+'''Fetch ghaar user from db'''
 
 
-def get(conn, group_id, user_id):
+def get_ghaar_user(conn, group_id, user_id):
     with conn.cursor() as cur:
-        rows_affected = cur.execute(
-            f"SELECT * from group_users WHERE group_id={group_id} and user_id={user_id};")
-        # check numnber of rows affected
+        cur.execute(
+            f"SELECT * FROM group_users WHERE group_id={group_id} and user_id={user_id};")
         return cur.fetchone()
 
 
-@commit
-def insert(conn, group_id, user_id):
+'''Fetch ghaar user from db by id'''
+
+
+def get_ghaar_user_by_id(conn, user_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"SELECT * FROM group_users WHERE user_id={user_id};")
+        return cur.fetchall()
+
+
+'''Fetch group ghaar users from db'''
+
+
+def get_group_ghaar_users(conn, group_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"SELECT * FROM group_users WHERE group_id={group_id} ORDER BY count DESC;")
+        return cur.fetchall()
+
+
+'''Create a new ghaar user in group'''
+
+
+def insert_ghaar_user(conn, group_id, user_id, user_name):
     with conn.cursor() as cur:
         try:
-            rows_affected = cur.execute(
-                f"INSERT INTO group_users(group_id,user_id,count) VALUES ({group_id},{user_id},0);")
+            cur.execute(
+                f"INSERT INTO group_users(group_id,user_id,user_name,count) VALUES ({group_id},{user_id},'{user_name}',0);")
+            conn.commit()
         except:
-            print(f"Failed to add ({group_id},{user_id}) to database!")
+            print(f"Failed to add user({group_id},{user_id}) to database!")
+            traceback.print_exc()
 
 
-@commit
-def increase(conn, group_id, user_id):
+'''Increases ghaar count for a user in group. If user does not exist in db, it creates the user automatically.'''
+
+
+def increase_ghaar_count(conn, group_id, user_id):
     with conn.cursor() as cur:
-        rows_affected = cur.execute(
+        cur.execute(
             f"UPDATE group_users SET count=count+1 WHERE group_id={group_id} and user_id={user_id};")
+        conn.commit()
 
 
-@commit
-def delete(conn, group_id, user_id):
+'''Deletes a ghaar user from db.'''
+
+
+def delete_ghaar_user(conn, group_id, user_id):
     with conn.cursor() as cur:
-        rows_affected = cur.execute(
-            f"DELETE from group_users WHERE group_id={group_id} and user_id={user_id};")
+        cur.execute(
+            f"DELETE FROM group_users WHERE group_id={group_id} and user_id={user_id};")
+        conn.commit()
 
 
-# conn = connect('localhost', 'root', 'Amirparsa96', 'Ghaar')
-# cur = conn.cursor()
-# delete(conn, 4, 6)
+'''Fetchs a ghaar message in group.'''
 
-# conn.close()
+
+def get_ghaar_message(conn, group_id, message_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"SELECT * FROM ghaar_messages WHERE group_id={group_id} and message_id={message_id};")
+        return cur.fetchone()
+        conn.commit()
+
+
+'''Fatchs all of ghaar messages in group.'''
+
+
+def get_ghaar_messages_by_group_id(conn, group_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"SELECT * FROM ghaar_messages WHERE group_id={group_id};")
+        return cur.fetchall()
+
+
+'''Adds a new ghaar message in group.'''
+
+
+def insert_ghaar_message(conn, group_id, message_id):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(
+                f"INSERT INTO ghaar_messages(group_id,message_id) VALUES ({group_id},{message_id});")
+            conn.commit()
+        except:
+            print(
+                f"Failed to add message({group_id},{message_id}) to database!")
+            traceback.print_exc()
+
+
+'''Deletes a ghaar message from group.'''
+
+
+def delete_ghaar_message(conn, group_id, message_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            "DELETE FROM ghaar_messages WHERE group_id={group_id} and message_id={message_id};")
+        conn.commit()
+
+
+'''Fetches a forwarded message in group'''
+
+
+def get_forwarded_message(conn, group_id, message_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"SELECT * FROM messages WHERE group_id={group_id} and message_id={message_id};")
+        return cur.fetchone()
+        conn.commit()
+
+
+'''Fetches all forwarded messages by group id'''
+
+
+def get_all_forwarded_messages(conn, group_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"SELECT * FROM messages WHERE group_id={group_id};")
+        return cur.fetchall()
+
+
+'''Adds a forwarded message to db'''
+
+
+def insert_forwarded_message_by_data(conn, group_id, message_id, caption, image_hash, video_hash):
+    with conn.cursor() as cur:
+        try:
+            cur.execute(f"INSERT INTO messages(group_id,message_id,caption,image_hash,video_hash) VALUES \
+                ({group_id},{message_id},'{caption}','{image_hash}','{video_hash}')")
+            conn.commit()
+        except:
+            print(
+                f"Failed to add forwarded message ({group_id},{message_id},{caption},{image_hash},{video_hash})")
+            traceback.print_exc()
+
+
+'''Adds a forwarded message to db'''
+
+
+def insert_forwarded_message(conn, message: ForwardedMessage):
+    insert_forwarded_message_by_data(conn, message.group_id, message.message_id,
+                                     message.caption, message.image_hash, message.video_hash)
+
+
+'''Deletes forwarded message from db'''
+
+
+def delete_forwarded_message(conn, group_id, message_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"DELETE FROM messages WHERE group_id={group_id} and message_id={message_id};")
+    conn.commit()
+
+
+'''deletes forwarded message by group_id'''
+
+
+def delete_forwarded_messages_by_group_id(conn, group_id):
+    with conn.cursor() as cur:
+        cur.execute(
+            f"DELETE FROM messages WHERE group_id={group_id};")
+    conn.commit()
--- main.py
+++ main.py
@@ -1,1 +1,143 @@
-print('hello')
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+import db
+import requests
+import json
+from PIL import Image
+import logging
+import comparator
+
+
+
+TOKEN = '1769102250:AAEAEww2WgyvfKMq4nOfbLy61JR55rAcnzk'
+
+updater = Updater(token=TOKEN, use_context=True)
+bot = telegram.Bot(TOKEN)
+
+ghaar_rejects = []
+
+conn = db.connect('localhost', 'root', 'sajadcr7', 'Ghaar')
+#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s-%(name)s-%(levelname)s-%(message)s')
+
+def get_markdown_call(name, id):
+    return name
+    # return f"[{name}](tg://user?id={id})"
+
+
+def is_ghaar(text):
+    text = f'{text[0]}'.join(
+        [text[i] for i in range(1, len(text)) if text[i - 1] != text[i]])
+    return text == "اغر"
+
+
+def button(update: telegram.Update, context: telegram.ext.CallbackContext):
+    query = update.callback_query
+    ghaar_rejects.append(query.from_user.id)
+    query.edit_message_text(text='غار بمولا\n' + str(len(ghaar_rejects)) + 'گفتن غار نیس',
+                            reply_markup=InlineKeyboardMarkup([[
+                                InlineKeyboardButton('غار نبود',callback_data='1')]]), parse_mode="Markdown")
+
+def forward_handler(update: telegram.Update, context: telegram.ext.CallbackContext):
+    group_id = update.message.chat_id
+    chat_id=update.message.message_id
+
+    text=""
+    try:
+        text = update.message.text
+    except:
+        pass
+    imgpic_id=update.message.photo[-1].file_id
+    picurl=f"https://api.telegram.org/bot{TOKEN}/getFile?file_id={imgpic_id}"
+    tt=requests.get(picurl)
+    tt=tt.json()
+    file_path=tt["result"]["file_path"]
+    pic_to_download=f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    imgpic=Image.open(requests.get(pic_to_download, stream=True).raw)
+    hash_img=comparator.gethash_by_image(imgpic)
+    all_forwarded=db.get_all_forwarded_messages(conn,group_id)
+    if len(all_forwarded)==0:
+        db.insert_forwarded_message_by_data(conn,group_id,chat_id,text,hash_img,"")
+    else:
+        ghaar_flag=False
+        ghaar_message_id=""
+        for forwarded_message in all_forwarded:
+            second_hash=forwarded_message[3]
+            #print(second_hash)
+            if comparator.compare_pics_hash(hash_img,second_hash):
+                ghaar_flag=True
+                ghaar_message_id=forwarded_message[1]
+                break
+            else:
+                pass
+        if ghaar_flag:
+            update.message.reply_text(text="I think YOU ARE GHAAAR!")
+            context.bot.send_message(chat_id=group_id,text="Inam madrak",reply_to_message_id=ghaar_message_id)
+        else:
+            db.insert_forwarded_message_by_data(conn, group_id, chat_id, text, hash_img, "")
+
+
+
+
+
+def text_handler(update: telegram.Update, context: telegram.ext.CallbackContext):
+    text = update.message.text
+    if is_ghaar(text):
+        chat_id = update.message.chat_id
+        ghaar_user_id = update.message.reply_to_message.from_user.id
+        ghaar_user_name = update.message.reply_to_message.from_user.full_name
+        ghaar_message_id = update.message.reply_to_message.message_id
+        if ghaar_user_id == 1769102250:
+            update.message.reply_text(text='داش ایسگامونو نگیر')
+        elif ghaar_user_id == 354614756:
+            update.message.reply_text(text='حاجی این که کلا غاره تنهایی فک کردی؟')
+        else:
+            if db.get_ghaar_message(conn, chat_id, ghaar_message_id) is None:
+                if db.get_ghaar_user(conn, chat_id, ghaar_user_id) is None:
+                    db.insert_ghaar_user(
+                        conn, chat_id, ghaar_user_id, ghaar_user_name)
+                db.increase_ghaar_count(conn, chat_id, ghaar_user_id)
+                db.insert_ghaar_message(conn, chat_id, ghaar_message_id)
+                keyboard = [[InlineKeyboardButton('غار نبود', callback_data='1')]]
+                mark_up = InlineKeyboardMarkup(keyboard)
+                update.message.reply_to_message.reply_text(text="غار بمولا",
+                                                           reply_markup=mark_up,
+                                                           parse_mode="Markdown")
+            else:
+                update.message.reply_to_message.reply_text(
+                    text="انقد غار بودی نفهمیدی این غار بوده!")
+
+
+def start(update: telegram.Update, context: telegram.ext.CallbackContext):
+    update.message.reply_text(text='به ربات غارگیران راه پیرو خوش آمدید')
+
+
+def stats_person(update: telegram.Update, context: telegram.ext.CallbackContext):
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
+    result = db.get_ghaar_user(conn, chat_id, user_id)
+    if result is None:
+        update.message.reply_text(
+            text="تا حالا تو این گروه غار نبودی!")
+    else:
+        update.message.reply_text(
+            text=f"تا حالا{result[3]} بار غاار بودی تو این گروه!")
+
+
+def stats_group(update: telegram.Update, context: telegram.ext.CallbackContext):
+    chat_id = update.message.chat_id
+    result = db.get_group_ghaar_users(conn, chat_id)
+    message = "سربازان گمنام پیرو\n"
+    for i, tup in enumerate(result):
+        message += f"{i + 1}. {get_markdown_call(tup[2], tup[1])} : {tup[3]} غاااار! \n"
+    update.message.reply_text(text=message)
+
+
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CallbackQueryHandler(button))
+dispatcher.add_handler(MessageHandler(Filters.reply, text_handler))
+dispatcher.add_handler(MessageHandler(Filters.forwarded, forward_handler))
+dispatcher.add_handler(CommandHandler("stats_group", stats_group))
+dispatcher.add_handler(CommandHandler("stats_person", stats_person))
+dispatcher.add_handler(CommandHandler("start", start))
+updater.start_polling()
